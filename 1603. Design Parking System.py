d3873b6a791fc9563341e92f11e525eefcb0f466
Kunal Mandil <kunalmandil47@gmail.com>
2021-04-27 01:49:37 -0400

    initial commit

--- 1603. Design Parking System.py
+++ 1603. Design Parking System.py
@@ -38,7 +38,7 @@ class ParkingSystem:
         pass
     def addCar(self, carType: int) -> bool:
         pass
-    
+
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
 # param_1 = obj.addCar(carType)
