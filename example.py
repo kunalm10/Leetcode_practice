6152d7ec0440d70e3cfe6188c958b582edce8e1a
Kunal Mandil <kunalmandil47@gmail.com>
2021-05-06 19:56:26 -0400

    initial commit

--- example.py
+++ example.py
@@ -2,13 +2,30 @@
 
 
 
+#
+# numbers = ['a', 'b', 'c', 'd']
+#
+# index = 0
+# for num in numbers:
+#     print(index , num)
+#     index += 1
+#
+# for index, num in enumerate(numbers):
+#     print(index, num)
+#
+#
+def hi(take_num):
+    def hi2(a,b):
+        print('hi1')
+        take_num(a,b)
+        print('hi2')
+    return hi2
 
-numbers = ['a', 'b', 'c', 'd']
+@hi
+def take_num(a,b):
+    print(a>b)
 
-index = 0
-for num in numbers:
-    print(index , num)
-    index += 1
-
-for index, num in enumerate(numbers):
-    print(index, num)
+# take_num = hi(take_num)
+a = 1
+b = 2
+take_num(a,b)
