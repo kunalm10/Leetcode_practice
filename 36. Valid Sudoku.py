be5fad3016599db0c706700f155841893d6a28f0
Kunal Mandil <kunalmandil47@gmail.com>
2021-06-02 15:53:58 -0400

    initial commit

---
+++ 36. Valid Sudoku.py
@@ -0,0 +1,125 @@
+"""
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+
+Example 1:
+
+
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+
+"""
+
+
+# Method 1
+def isValidSudoku(board):
+    for i in range(9):
+        print(board[i])
+        discovered_numbers_row = []
+        for num in board[i]:
+            if num in discovered_numbers_row and num != '.':
+                return False
+            else:
+                discovered_numbers_row.append(num)
+    for i in range(9):
+        discovered_numbers_col = []
+        for j in range(9):
+            num = board[j][i]
+            if num in discovered_numbers_col and num != '.':
+                return False
+            else:
+                discovered_numbers_col.append(num)
+    for k in range(3):
+        for l in range(3):
+            discovered_numbers_box = []
+            for i in range(3):
+                for j in range(3):
+                    num = board[i + 3 * k][j + 3 * l]
+                    if num in discovered_numbers_box and num != '.':
+                        return False
+                    else:
+                        discovered_numbers_box.append(num)
+    return True
+
+
+# Method 2
+def isValidSudoku(board):
+    rows_count = [set() for _ in range(9)]
+    columns = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
+    for i, rows in enumerate(board):
+        for j, num in enumerate(rows):
+            if num != '.':
+                box_num = (i // 3) * 3 + j // 3
+                if num in rows_count[i] or num in columns[j] or num in boxes[box_num]:
+                    return False
+                else:
+                    rows_count[i].add(num)
+                    columns[j].add(num)
+                    boxes[box_num].add(num)
+    return True
+
+
+if __name__ == '__main__':
+    board = \
+        [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+            , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+            , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+            , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+            , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+            , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+            , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+            , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+            , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+    print(isValidSudoku(board))
+
+    board = \
+        [["8", "3", ".", ".", "7", ".", ".", ".", "."]
+            , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+            , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+            , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+            , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+            , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+            , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+            , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+            , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+    print(isValidSudoku(board))
